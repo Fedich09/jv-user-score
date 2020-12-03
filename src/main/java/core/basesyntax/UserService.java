af0dd7abd070546bb97e7c921484aee44b74e679
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-03 17:44:09 +0200

    fixed remarks

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -8,13 +8,9 @@ public class UserService {
         for (int i = 0; i < records.length; i++) {
             if (records[i].substring(0, records[i].indexOf(":")).equals(email)) {
                 email = records[i].substring(records[i].indexOf(":") + 1);
+                return Integer.parseInt(email);
             }
         }
-        try {
-            Integer.parseInt(email);
-        } catch (NumberFormatException e) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return Integer.parseInt(email);
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
